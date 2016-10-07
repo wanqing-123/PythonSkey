245ff412ff7b2a180dc93491b94e256666d27a21
wanqing-123 <31007354@qq.com>
2016-10-07 21:14:24 +0800

    FileUtils.py

--- test/PTeset.py
+++ test/PTeset.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
+
 print 'Python OS'
+
+jsonstr = {"key1": "value1", "key2": "value2"}
+print json.dumps(jsonstr)
+
+def mainf():
+    print 'main'
+
+if __name__ == '__main__':
+    mainf()
