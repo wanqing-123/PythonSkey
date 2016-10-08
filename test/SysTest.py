2e274fff22e47203b66c19c7fb2f1b1363fe7a3b
wanqing-123 <31007354@qq.com>
2016-10-08 10:55:14 +0800

    sys, getopt模块

---
+++ test/SysTest.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# by ayalamih on 8 Oct. 2016
+# 
+
+import sys, getopt
+
+print len(sys.argv)
+print sys.argv
+
+def main(argv):
+    print "argv:", argv
+
+    inputfile = ""
+    outputfile = ""
+    targetfile = ""
+
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:t:",["infile=", "outfile=", "target="])
+        print "opts:", opts
+        print "args:", args
+    except getopt.GetoptError:
+        print 'usage: -i -o -t'
+        print '     : --infile= --outfile= --target='
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == "-h":
+            print 'usage: -i  -o -t'
+            print '     : --infile= --outfile= --target='
+            sys.exit()
+        elif opt in ("-i", "--infile"):
+            inputfile = arg
+        elif opt in ("-o", "--outfile"):
+            outputfile = arg
+        elif opt in ("-t", "--target"):
+            targetfile = arg
+
+    print 'Input File : ', inputfile
+    print 'Out File : ', outputfile
+    print 'Target File : ', targetfile
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
